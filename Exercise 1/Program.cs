611ac3d6e3696ed485bb46e0dd991c59aafed120
Vivekkumar260892 <vivekkumar1@mail.usf.edu>
2019-01-21 16:08:14 -0500

    Exercise 2

--- Exercise 1/Program.cs
+++ Exercise 1/Program.cs
@@ -1,28 +1,28 @@
 ﻿using System;
 
-namespace Exercise_1
+namespace Exercise 2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input side a : ");
-            string Input_1 = Console.ReadLine();
-            double side_a = double.Parse(Input_1);
-            Console.WriteLine("Input side b : ");
-            string Input_2 = Console.ReadLine();
-            double side_b = double.Parse(Input_2);
-            Console.WriteLine("Input side c : ");
-            string Input_3 = Console.ReadLine();
-            double side_c = double.Parse(Input_3);
-            double s = (side_a + side_b + side_c)/2;
-            Console.WriteLine("s = "+s);
-            double area=Math.Sqrt(s*(s- side_a)*(s - side_b)*(s - side_c));
-            Console.WriteLine("Area is : "+ area);
+            // Exercise 2// Input a number (n) from the user and check if it is even or odd.// Write your code here
+            Console.WriteLine("Input a number : ");
+            string Input = Console.ReadLine();
+            int numbr_1 = int.Parse(Input);
+            if (numbr_1 % 2 == 0)
+            {
+                Console.WriteLine("Even number");
+            }
+            else
+            {
+                Console.WriteLine("odd number");
+            }
             Console.ReadKey();
-            // Exercise 1// Calculate the area of a triangle using Heron's formula
-            // Area = SquareRoot(s * (s-a) * (s-b) * (s-c)) where s=(a+b+c)/2 and 
-            //a,b,c are the sides of the triangle// Eg. a=3, b=4, c=5. Area = 6
+
+
+
+
         }
     }
 }
